25a447bc8749017029aaab337f810794d4e9adcc
Manojkumar M <mmanojkumar@athenahealth.com>
2021-09-30 01:17:13 +0530

    To display loading progress during login and signup

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -1,6 +1,9 @@
 import React, { Component, useState } from "react";
 import OAuthLogin from "./login.google";
 import axios from "axios";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import BlockUi from "react-block-ui";
+import "react-block-ui/style.css";
 
 export interface LoginProps {
     isOnDashboard: CallableFunction,
@@ -8,8 +11,10 @@ export interface LoginProps {
 }
 
 const Login: React.FC<LoginProps> = ({ isOnDashboard, setLoginType }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const handleSubmit = (event: any): any => {
         event.preventDefault();
+        setIsLoading(true);
         const email = event.target.elements.email.value;
         const password = event.target.elements.password.value;
         console.log({ email: email, password: password });
@@ -17,6 +22,7 @@ const Login: React.FC<LoginProps> = ({ isOnDashboard, setLoginType }) => {
             email: email,
             password: password
         }).then(resp => {
+            setIsLoading(false);
             console.log(resp);
             isOnDashboard();
         });
@@ -24,40 +30,42 @@ const Login: React.FC<LoginProps> = ({ isOnDashboard, setLoginType }) => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <h3>Sign In</h3>
+            <BlockUi tag="div" blocking={isLoading}>
+                <form onSubmit={handleSubmit}>
+                    <h3>Sign In</h3>
 
-                <div className="form-group form-margin">
-                    <label>Email address</label>
-                    <input id="email" type="email" className="form-control" placeholder="Enter email" />
-                </div>
+                    <div className="form-group form-margin">
+                        <label>Email address</label>
+                        <input id="email" type="email" className="form-control" placeholder="Enter email" required />
+                    </div>
 
-                <div className="form-group form-margin">
-                    <label>Password</label>
-                    <input id="password" type="password" className="form-control" placeholder="Enter password" />
-                </div>
+                    <div className="form-group form-margin">
+                        <label>Password</label>
+                        <input id="password" type="password" className="form-control" placeholder="Enter password" required />
+                    </div>
 
-                <div className="form-group form-margin">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input checkbox-margin" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    <div className="form-group form-margin">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input checkbox-margin" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </form>
+                <div className="divider-container">
+                    <p className="divider-text">OR</p>
+                    <hr className="ruler-margin" />
+                    <div className="google-login-container">
+                        <OAuthLogin
+                            isOnDashboard={isOnDashboard}
+                            setLoginType={setLoginType} />
                     </div>
                 </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-            <div className="divider-container">
-                <p className="divider-text">OR</p>
-                <hr className="ruler-margin" />
-                <div className="google-login-container">
-                    <OAuthLogin
-                        isOnDashboard={isOnDashboard}
-                        setLoginType={setLoginType} />
-                </div>
-            </div>
+            </BlockUi>
         </div>
     );
 }
--- src/components/signup/signup.component.tsx
+++ src/components/signup/signup.component.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import BlockUi from "react-block-ui";
+import "react-block-ui/style.css";
 
 export interface SignUpProps {
     isOnDashboard: CallableFunction
 }
 const SignUp: React.FC<SignUpProps> = ({ isOnDashboard }) => {
+    const [isLoading, setIsLoading] = useState(false);
     let history = useHistory();
     const handleSubmit = (event: any): any => {
         event.preventDefault();
+        setIsLoading(true);
         const firstname = event.target.elements.firstname.value;
         const lastname = event.target.elements.lastname.value;
         const email = event.target.elements.email.value;
@@ -20,39 +24,44 @@ const SignUp: React.FC<SignUpProps> = ({ isOnDashboard }) => {
             email: email,
             password: password
         }).then(resp => {
+            setIsLoading(false);
             console.log(resp);
             history.push('/sign-in');
         });
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <h3>Sign Up</h3>
+        <div>
+            <BlockUi tag="div" blocking={isLoading}>
+                <form onSubmit={handleSubmit}>
+                    <h3>Sign Up</h3>
 
-            <div className="form-group form-margin">
-                <label>First name</label>
-                <input id="firstname" type="text" className="form-control" placeholder="First name" />
-            </div>
+                    <div className="form-group form-margin">
+                        <label>First name</label>
+                        <input id="firstname" type="text" className="form-control" placeholder="First name" required />
+                    </div>
 
-            <div className="form-group form-margin">
-                <label>Last name</label>
-                <input id="lastname" type="text" className="form-control" placeholder="Last name" />
-            </div>
+                    <div className="form-group form-margin">
+                        <label>Last name</label>
+                        <input id="lastname" type="text" className="form-control" placeholder="Last name" required />
+                    </div>
 
-            <div className="form-group form-margin">
-                <label>Email address</label>
-                <input id="email" type="email" className="form-control" placeholder="Enter email" />
-            </div>
+                    <div className="form-group form-margin">
+                        <label>Email address</label>
+                        <input id="email" type="email" className="form-control" placeholder="Enter email" required />
+                    </div>
 
-            <div className="form-group form-margin">
-                <label>Password</label>
-                <input id="password" type="password" className="form-control" placeholder="Enter password" />
-            </div>
+                    <div className="form-group form-margin">
+                        <label>Password</label>
+                        <input id="password" type="password" className="form-control" placeholder="Enter password" required />
+                    </div>
 
-            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-            <p className="forgot-password text-right">
-                Already registered <a href="/sign-in">sign in?</a>
-            </p>
-        </form>
+                    <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                    <p className="forgot-password text-right">
+                        Already registered <a href="/sign-in">sign in?</a>
+                    </p>
+                </form>
+            </BlockUi>
+        </div>
     );
 }
 
